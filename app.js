ac8ab9c2b15a5c5096e2ad74302fa10a938cb2e1
gitsibi <159125779+gitsibi@users.noreply.github.com>
2024-03-06 14:17:35 +0530

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const playButton = document.getElementById("play-button");
+
+    playButton.addEventListener("click", function() {
+        // Show the game page
+        window.location.href = "game.html";
+    });
+});
+
